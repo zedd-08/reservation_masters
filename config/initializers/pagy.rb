632ba86db53473d2ddbecd0c9cc8ae58a6bcaa3d
zedd-08 <manish.96@live.com>
2024-06-25 18:00:53 +0200

    Added pagination to stays list

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,5 @@
 # Optionally override some pagy default with your own in the pagy initializer
-Pagy::DEFAULT[:items] = 10 # items per page
+Pagy::DEFAULT[:items] = 5 # items per page
 Pagy::DEFAULT[:size]  = [1, 4, 4, 1] # nav bar links
 # Better user experience handled automatically
 require 'pagy/extras/overflow'
