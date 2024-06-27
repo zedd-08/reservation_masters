40d802fa35e0b7f7033a7d258e67f656d8d38ff4
zedd-08 <manish.96@live.com>
2024-06-27 16:50:59 +0200

    Updated to correctly pick the reservation_id from request params

--- app/controllers/generate_receipts_controller.rb
+++ app/controllers/generate_receipts_controller.rb
@@ -1,15 +1,22 @@
-
 class GenerateReceiptsController < ApplicationController
+  before_action :set_reservation
+
   def generate_pdf
-    reservation = Reservation.last
-    stay = Stay.find(reservation.stay_id)
+    stay = Stay.find(@reservation.stay_id)
     receipt_service = ReceiptGeneratorService.new
-    receipt_content = receipt_service.generate_pdf(reservation, stay)
-    receipt_filename = "invoices/#{stay.id}-#{reservation.id}-#{reservation.name.downcase.gsub(' ', '_')}.pdf"
+    receipt_content = receipt_service.generate_pdf(@reservation, stay)
+    receipt_filename = "#{@reservation.name.downcase.gsub(" ", "_")}-#{stay.id}-#{@reservation.id}.pdf"
 
-    # PdfMailer.send_report.deliver_now
-    File.open(receipt_filename, 'wb') { |file| file.write(receipt_content) }
+    send_data receipt_content, filename: receipt_filename, type: 'pdf', disposition: 'attachment'
 
-    send_data receipt_content, filename: receipt_filename, type: 'pdf', disposition: 'inline'
+    # File.open(receipt_filename, "wb") { |file| file.write(receipt_content) }
+    # send_file receipt_filename, type: "application/pdf", disposition: "inline"
   end
+
+  private
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, notice: "Reservation not found"
+    end
 end
