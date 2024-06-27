b18f106acf65b85e3a6990ca4e27096e9c8dbfc7
zedd-08 <manish.96@live.com>
2024-06-27 20:40:05 +0200

    Try to fix tailwind/scss rgb vs RGB issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.assets.css_compressor = nil
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
