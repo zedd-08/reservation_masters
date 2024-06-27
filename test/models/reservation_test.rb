87873340da95a04f603e288a2fe09c6c66ce711f
zedd-08 <manish.96@live.com>
2024-06-27 21:37:08 +0200

    Updated basic reservation model tests

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "reservation must contain required attributes" do
+    reservation = Reservation.new
+
+    assert reservation.invalid?
+
+    assert reservation.errors[:name].any?
+    assert reservation.errors[:address].any?
+    assert reservation.errors[:email].any?
+    assert reservation.errors[:check_in].any?
+    assert reservation.errors[:check_out].any?
+    assert reservation.errors[:pay_type].any?
+  end
+
+  test "check in should be before check out" do
+    reservation = reservations(:one)
+    assert reservation.valid?
+
+    reservation.check_out = reservation.check_in.yesterday
+
+    assert reservation.invalid?
+  end
 end
